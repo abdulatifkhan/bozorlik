ff07aee1f14ec8234d559b28394f82a1c8931830
abdulatifkhan <anvarkhanov96@gmail.com>
2020-11-10 08:49:50 +0500

    Add project "Bozorlik"

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+var elShoppingListForm = document.querySelector('.shopping-list-form');
+var elShoppingListInput = document.querySelector('.shopping-list-input');
+var elShoppingList = document.querySelector('.shopping-list');
+
+var list = [];
+
+elShoppingListForm.addEventListener('submit', function (evt) {
+  evt.preventDefault(); 
+
+  var newLiIteam = document.createElement('li');
+  newLiIteam.classList.add('list-group-item', 'list-group-item-action');
+
+  list.push(elShoppingListInput.value);
+
+  newLiIteam.textContent = elShoppingListInput.value;
+  elShoppingListInput.value = '';
+ 
+  elShoppingList.appendChild(newLiIteam);
+});
+
+// Check list
+
+
+var elCheckList = document.querySelector('.check-list-form');
+var elCheckInput = document.querySelector('.check-list-input');
+
+elCheckList.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+
+  // var key = ;
+  elCheckInput.value = '';
+  if(list.includes(elCheckInput.value.trim())) {
+    alert("Ha mavjud");
+  } else {
+    alert("Ro'hatda yo'q");
+  }
+}) 
+
